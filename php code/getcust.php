c5792c246ce5eceb823ebb36d6fc375dabfe33f2
Jaisen Bynoe <jaibynoe@gmail.com>
2015-11-26 15:28:28 -0500

    jaisen is cute

--- php code/getcust.php
+++ php code/getcust.php
@@ -4,9 +4,10 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
     die("Failed to retreive customer.");
 }
-
+echo "<select>"
 while ($row = mysqli_fetch_assoc($result)) {
-     echo $row["genre"]. " ". $row["name"]."</option>";
+     echo "<option>".$row["genre"]. " ". $row["name"]."</option>";
 }
+echo "</select>"
 mysqli_free_result($result);
 ?>
