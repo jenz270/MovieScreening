363e7982e5d1fc4eefc045596dd222bd4c3617df
Jaisen Bynoe <jaibynoe@gmail.com>
2015-11-28 15:38:25 -0500

    needs a bit of review

---
+++ php/getMovieListByMovieTitle.php
@@ -0,0 +1,13 @@
+<?php
+
+$movieName = $_POST["moviename"];
+$query ='select moviename, capacity, count(selected.custID) as total from movie left join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID left join theatre on showing.roomnum = theatre.roomnum where moviename ="'. $movieName.'"  group by showing.showID having total < capacity';
+$result = mysqli_query($connection,$query);
+if (!$result) {
+    die("Failed to retreive Movie Information");
+}
+while ($row = mysqli_fetch_assoc($result)) {
+     echo $row["genre"];
+}
+mysqli_free_result($result);
+?>
