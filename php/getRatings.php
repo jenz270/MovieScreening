7ff7589f2ba868cfacaf0cb4e3da3ac242f8ed63
Jaisen Bynoe <jaibynoe@gmail.com>
2015-11-27 21:07:11 -0500

    adding an additional statement you beauty ;)

--- php/getRatings.php
+++ php/getRatings.php
@@ -1,5 +1,5 @@
 <?php
-$query = "select moviename,avg(rate) as total from movie join showing on movie.movieID = showing.movieID join selected on showing.showID = selected.showID group by moviename";
+$query = "select moviename,avg(rate) as total from movie join showing on movie.movieID = showing.movieID join selected on showing.showID = selected.showID group by moviename having total >=4";
 $result = mysqli_query($connection,$query);
 if (!$result) {
     die("Failed to retreive ratings.");
