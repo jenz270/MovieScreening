6afb81937af7d15f53387d94146d0dfe891308bd
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-01 17:02:38 -0500

    I FAIL AT THIS NO!!!!!!

--- php/getMovieListByDateRange.php
+++ php/getMovieListByDateRange.php
@@ -1,6 +1,6 @@
 
 <?php
-    $query = "select moviename,theatre.roomnum,capacity, count(selected.custID) as total from movie join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID join theatre on showing.roomnum = theatre.roomnum where showing.show_date between '2015-03-20' and '2015-03-22' group by moviename, showing.showID having total < capacity";
+    $query = "select moviename, year,theatre.roomnum,capacity, count(selected.custID) as total from movie join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID join theatre on showing.roomnum = theatre.roomnum where showing.show_date between '2015-03-20' and '2015-03-22' group by moviename, showing.showID having total < capacity";
     $result = mysqli_query($connection,$query);
     if(!result){
         die(" Could not load movies.");
--- php/getMovieListByGenre.php
+++ php/getMovieListByGenre.php
@@ -1,7 +1,7 @@
 <?php
 
 $genre = $_POST["genre"];
-$query ='select moviename, capacity, count(selected.custID) as total from genre join movie on genre.movieID = movie.movieID left join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID left join theatre on showing.roomnum = theatre.roomnum where genre ="'. $genre.'"  group by showing.showID having total < capacity';
+$query ='select moviename,year, capacity, count(selected.custID) as total from genre join movie on genre.movieID = movie.movieID left join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID left join theatre on showing.roomnum = theatre.roomnum where genre ="'. $genre.'"  group by showing.showID having total < capacity';
 $result = mysqli_query($connection,$query);
 if (!$result) {
     die("Failed to retreive Movie Information");
