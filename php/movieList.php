47273cf603ca6d522aac0561ab3c73644311267c
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-03 14:46:37 -0500

    for genre

---
+++ php/movieList.php
@@ -0,0 +1,20 @@
+ <?php
+        include 'connectdb.php';
+   ?>
+
+ <form action="getMovieListByGenre.php" method="post">
+             Genre: 
+             <select name="genreList">
+                    <option value="Comedy">Comedy</option>
+                    <option value="Action">Action</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Suspense">Suspense</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Romance">Romance</option>
+                    <option value="Documentary">Documentary</option>
+              </select>
+              <br>
+        <input type="submit" value="genre">
+        </form>
