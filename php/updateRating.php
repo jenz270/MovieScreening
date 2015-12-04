421d35c2d043358c6ac77da1d3c89b1bc68a40f6
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-04 12:46:14 -0500

    done basic functionality..... now need to enter when empty and error handling

--- php/updateRating.php
+++ php/updateRating.php
@@ -4,16 +4,25 @@ include 'connectdb.php';
 
 
 $customerName = $_POST["customerList"];
+
 $movieName = $_POST["movieList"];
+
 $rate = $_POST["rating"];
+
 $query = "update selected join customer on selected.custID = customer.custID join showing on selected.showID = showing.showID join movie on showing.movvieID = movie.movieID set rate ='".$rate. "' where movie.moviename ='" .$$movieName. "' and  customer.name = '".$customerName. "'";
+
 $result = mysqli_query($connection,$query);
+
 if (!$result) {
     die("Failed to retreive Customer Information");
 }
-while ($row = mysqli_fetch_assoc($result)) {
-     echo "Successfully updated!";
-}
-mysqli_free_result($result);
+ if (mysqli_query($connection, $query)) {
+      echo "Updated Successfully";
+   } else {
+      echo "Failed to update for Customer " . mysqli_close(connection);
+   }
+
+mysqli_close($connection);
+
 ?>
 
