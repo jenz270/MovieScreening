872107bc439a2a030b2c4242599b919cb20db81c
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-03 20:08:49 -0500

    Just needs to be made to look good

--- php/getMovieListByMovieTitle.php
+++ php/getMovieListByMovieTitle.php
@@ -1,13 +1,16 @@
 <?php
+include 'connectdb.php';
+?>
 
-$movieName = $_POST["moviename"];
-$query ='select moviename,year, capacity, count(selected.custID) as total from movie left join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID left join theatre on showing.roomnum = theatre.roomnum where moviename ="'. $movieName.'"  group by showing.showID having total < capacity';
+<?php
+$movieName = $_POST["movieList"];
+$query ='select moviename,year,showing.roomnum, capacity, count(selected.custID) as total from movie left join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID left join theatre on showing.roomnum = theatre.roomnum where movie.movieID='. $movieName .' group by showing.showID having total < capacity';
 $result = mysqli_query($connection,$query);
 if (!$result) {
     die("Failed to retreive Movie Information");
 }
 while ($row = mysqli_fetch_assoc($result)) {
-     echo $row["moviename"];
+     echo $row["moviename"] . ','. $row["year"] .','. $row["roomnum"] ."</br>";
 }
 mysqli_free_result($result);
 ?>
