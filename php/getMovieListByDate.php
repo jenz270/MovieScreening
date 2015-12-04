29fa252303bd34e2a40789809b34aa92bb230fdf
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-03 21:13:53 -0500

    You are so cute ;) #WineThing

---
+++ php/getMovieListByDate.php
@@ -0,0 +1,20 @@
+<?php
+     include 'connectdb.php';
+
+?>
+
+<?php
+    $startdate = $_POST["startdate"];
+    $enddate = $_POST["enddate"];
+
+
+    $query = "select moviename, year,theatre.roomnum,capacity, count(selected.custID) as total from movie join showing on movie.movieID = showing.movieID left join selected on selected.showID = showing.showID join theatre on showing.roomnum = theatre.roomnum where showing.show_date between show_date='.$stardate.' and show_date='.$enddate.' group by moviename, showing.showID having total < capacity";
+    $result = mysqli_query($connection,$query);
+    if(!result){
+        die(" Could not load movies.");
+    }
+    while ($row = mysqli_fetch_assoc($result)) {
+     echo $row["moviename"]. ',' .$row["year"];
+    }
+    mysqli_free_result($result);
+?>
