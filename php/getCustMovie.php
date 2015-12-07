72fa2f37308bc4914302700a90e008bc47ddd890
Jaisen Bynoe <jaibynoe@gmail.com>
2015-12-07 17:00:34 -0500

    wow PINEAPPLES!!

---
+++ php/getCustMovie.php
@@ -0,0 +1,24 @@
+<!--
+  getMovieTitle.php gets all movies in the database
+-->
+<?php
+   include 'connectdb.php';
+
+   $cust = $_POST["customerList"];
+   $query = "select * from movie join left join showing on movie.movieID =showing.movieID join selected on showing.showID  = selected.showID where selected.custID='". $cust . "'";
+   $result = mysqli_query($connection,$query);
+   if (!$result) {
+        die("databases query failed.");
+    }
+   echo "</br>";
+   echo "<h3>". "Select a movie: " . "</h3>";
+   echo '<select name="'. "movieList".'">';
+   echo '<option value="' . " " . '">';
+   echo ' ' . "</option>";
+   while ($row = mysqli_fetch_assoc($result)) {
+       echo '<option value="' . $row["movieID"]. '">';
+        echo $row["moviename"]. "</option>";
+   }
+   echo "</select>";
+   mysqli_free_result($result);
+?>
